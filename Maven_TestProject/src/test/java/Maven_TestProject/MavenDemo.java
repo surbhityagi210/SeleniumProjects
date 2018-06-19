6648ab92c12b52b452e62d16b004950f202a67f7
tyagi.s <tyagi.s@DT-GANGWARA.Dataworld.co.in>
2018-06-19 11:14:26 +0530

    first commit

---
+++ Maven_TestProject/src/test/java/Maven_TestProject/MavenDemo.java
@@ -0,0 +1,13 @@
+package Maven_TestProject;
+
+import org.testng.annotations.Test;
+
+public class MavenDemo {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("Welcome to Maven Project.");
+	}
+
+}
